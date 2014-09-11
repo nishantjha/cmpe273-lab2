46bbbde8ef531abf2d4098edffc3b7a1e6bd90fe
Ubuntu <ubuntu@ip-172-31-27-131.us-west-1.compute.internal>
2014-09-11 07:38:38 +0000

    Third Commit

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var login = require('./login');
 
 var app = connect();
 
-app.use(connect.json()); // Parse JSON request body into `request.body`
-app.use(connect.urlencoded()); // Parse form in request body into `request.body`
-app.use(connect.cookieParser()); // Parse cookies in the request headers into `request.cookies`
-app.use(connect.query()); // Parse query string into `request.query`
+app.use(connect.json());
+app.use(connect.urlencoded());
+app.use(connect.cookieParser()); 
+app.use(connect.query());
 
 app.use('/', main);
 
@@ -36,29 +36,21 @@ function get(request, response) {
 };
 
 function post(request, response) {
-	// TODO: read 'name and email from the request.body'
-	// var newSessionId = login.login('xxx', 'xxx@gmail.com');
-	// TODO: set new session id to the 'session_id' cookie in the response
-	// replace "Logged In" response with response.end(login.hello(newSessionId));
-
+	
 	response.end("Logged In\n");
 };
 
 function del(request, response) {
 	console.log("DELETE:: Logout from the server");
- 	// TODO: remove session id via login.logout(xxx)
- 	// No need to set session id in the response cookies since you just logged out!
-
+ 	
   	response.end('Logged out from the server\n');
 };
 
 function put(request, response) {
-	console.log("PUT:: Re-generate new seesion_id for the same user");
-	// TODO: refresh session id; similar to the post() function
-
+	console.log("PUT:: Re-generate new session_id for the same user");
 	response.end("Re-freshed session id\n");
 };
 
 app.listen(8000);
 
-console.log("Node.JS server running at 8000...");
+console.log("Server(Node.js) running at 8000...");
