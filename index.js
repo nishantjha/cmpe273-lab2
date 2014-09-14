7ec12841e807902e1cbdbbabd97fe712e1799e89
Ubuntu <ubuntu@ip-172-31-27-131.us-west-1.compute.internal>
2014-09-14 02:21:01 +0000

    Fourth Commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = connect();
 
 app.use(connect.json());
 app.use(connect.urlencoded());
-app.use(connect.cookieParser()); 
+app.use(connect.cookieParser());
 app.use(connect.query());
 
 app.use('/', main);
@@ -22,35 +22,50 @@ function main(request, response, next) {
 function get(request, response) {
 	var cookies = request.cookies;
 	console.log(cookies);
-	if ('session_id' in cookies) {
-		var sid = cookies['session_id'];
-		if ( login.isLoggedIn(sid) ) {
-			response.setHeader('Set-Cookie', 'session_id=' + sid);
-			response.end(login.hello(sid));	
-		} else {
-			response.end("Invalid session_id! Please login again\n");
-		}
-	} else {
-		response.end("Please login via HTTP POST\n");
+	   if ('session_id' in cookies)
+      {
+	var sid = cookies['session_id'];
+        if ( login.isLoggedIn(sid) )
+        {
+	    response.setHeader('Set-Cookie', 'session_id=' + sid);
+	    response.end(login.hello(sid));	
 	}
+        else
+        {
+			response.end("Invalid session_id! Please login again\n");
+	}
+      }
+       else
+          {
+	         	response.end("Please login via HTTP POST\n");
+	  }
 };
 
 function post(request, response) {
-	
-	response.end("Logged In\n");
+	var newSessionId = login.login('Nishant Jha', 'nishant.jha@sjsu.edu');
+        var cookies = request.cookies;
+        response.setHeader('Set-Cookie', 'session_id=' + newSessionId);
+	cookies['session_id'] = newSessionId;
+        response.end(login.hello(newSessionId));
 };
 
 function del(request, response) {
 	console.log("DELETE:: Logout from the server");
- 	
+ 	var cookies=request.cookies;
+        var sid=cookies['session_id'];
+        login.logout(sid);
   	response.end('Logged out from the server\n');
 };
 
 function put(request, response) {
 	console.log("PUT:: Re-generate new session_id for the same user");
-	response.end("Re-freshed session id\n");
+	var newSessionId = login.login('Nishant Jha', 'nishant.jha@sjsu.edu');
+        var cookies = request.cookies;
+        response.setHeader('Set-Cookie', 'session_id=' + newSessionId);
+        cookies['session_id'] = newSessionId;
+       	response.end("Re-freshed session id\n");
 };
 
 app.listen(8000);
 
-console.log("Server(Node.js) running at 8000...");
+console.log("Node.JS server running at 8000...");
